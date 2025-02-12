73f228c3d13a66ec0293edf2ccf91b6c1f99b258
Katrina Ventura <145261408+elyzuh@users.noreply.github.com>
2025-02-12 16:10:59 +0800

    fixed warning

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main()
+        {
+            Deck deck = new Deck();
+            while (true)
+            {
+                Console.WriteLine("\nDeck of Cards - Menu:");
+                Console.WriteLine("1. Create Deck");
+                Console.WriteLine("2. Shuffle Deck");
+                Console.WriteLine("3. Deal Cards");
+                Console.WriteLine("4. Display Deck");
+                Console.WriteLine("5. Exit");
+                Console.Write("Choose an option: ");
+
+                string choice = Console.ReadLine() ?? string.Empty; 
+                switch (choice)
+                {
+                    case "1":
+                        deck.Create();
+                        break;
+                    case "2":
+                        deck.Shuffle();
+                        break;
+                    case "3":
+                        Console.Write("Enter number of cards to deal: ");
+                        if (int.TryParse(Console.ReadLine(), out int count))
+                        {
+                            deck.Deal(count);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid number.");
+                        }
+                        break;
+                    case "4":
+                        deck.Display();
+                        break;
+                    case "5":
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
